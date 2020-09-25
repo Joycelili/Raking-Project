792bb0870bc3117766fb7f2d635ac0ff16989189
Joycelili <joycelili@hotmail.com>
2020-09-25 11:03:58 +0200

    Revisar Random meme

--- src/controllers/lab_controllers.py
+++ src/controllers/lab_controllers.py
@@ -7,16 +7,17 @@ from src.app import app
 def create_lab(name):
     """Crea el nombre de un lab"""
     new_lab = {
-    'Lab': name}
+    'Title': name}
     result = db.pull.insert_one(new_lab)
     return {'_id': str(result.inserted_id)}
-
-@app.route('/lab/<lab>/meme')
-def random_meme(lab):
-    """Coge un meme random"""
+"""
+@app.route('/lab/<lab_name>/meme')
+def random_meme(lab_name):
+   
     result=db.pull_request.aggregate([
-        { "$match":  {"Lab": lab} },
+        { "$match":  {"Title": lab} },
         { "$sample": {"size": 1} },
-        { "$project": { "Url" : 1, "_id": 0}}
+        { "$project": { "Meme" : 1, "_id": 0}}
       ])
     return dumps(result)
+"""
